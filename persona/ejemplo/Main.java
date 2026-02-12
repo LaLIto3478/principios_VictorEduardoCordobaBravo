3781853af4d949307a1339cbff81713e605f3159
Víctor Eduardo Córdoba Bravo <zs24003957@estudiantes.uv.mx>
2026-02-12 08:29:04 -0600

    persona y television

---
+++ persona/ejemplo/Main.java
@@ -0,0 +1,43 @@
+
+
+/**
+ * Class Main
+ */
+public class Main {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public Main () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @param        args
+   */
+  public static void main(String[] args)
+  {
+	  System.out.println("Victor");
+	  System.out.println("19 años");
+	  Persona myPersona = new Persona("Veronica", 20);
+	  System.out.println(myPersona.saludar());
+  }
+
+
+}
