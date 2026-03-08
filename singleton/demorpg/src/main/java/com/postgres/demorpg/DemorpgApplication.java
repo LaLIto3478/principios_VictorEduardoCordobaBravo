672b286d5255ae9a33c1c0020eac89be3aea4b97
LaLIto3478 <zs24003957@estudiantes.uv.mx>
2026-03-08 09:49:27 -0600

    spring

---
+++ singleton/demorpg/src/main/java/com/postgres/demorpg/DemorpgApplication.java
@@ -0,0 +1,13 @@
+package com.postgres.demorpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorpgApplication.class, args);
+	}
+
+}
