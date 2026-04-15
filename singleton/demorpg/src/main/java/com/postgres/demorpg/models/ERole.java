aeaff3725a5848d0805afb2ba9f6116593afb428
LaLIto3478 <zs24003957@estudiantes.uv.mx>
2026-04-14 20:06:40 -0600

    k

---
+++ singleton/demorpg/src/main/java/com/postgres/demorpg/models/ERole.java
@@ -0,0 +1,7 @@
+package com.postgres.demorpg.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
--- singleton/demorpg/src/main/java/com/postgres/demorpg/models/Tweet.java
+++ singleton/demorpg/src/main/java/com/postgres/demorpg/models/Tweet.java
@@ -2,42 +2,76 @@ package com.postgres.demorpg.models;
 
 import java.util.HashSet;
 import java.util.Set;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
 @Table( name = "tweets")
+
 public class Tweet {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 
 	@NotBlank
 	@Size(max = 140)
 	private String tweet;
 
-	public Tweet() {}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "posted_by", referencedColumnName = "id")
+	private User postedBy;
 
-	public Tweet (String tweet){
+
+
+	public User getPostedBy() {
+		return postedBy;
+	}
+
+	public void setPostedBy(User postedBy) {
+		this.postedBy = postedBy;
+	}
+
+	public Tweet() {
+	}
+
+	public  Tweet (String tweet) {
 		this.tweet = tweet;
 	}
 
-	// GETTERS AND SETTERS
-	public Long getId(){
+	// getters and setters
+
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Long id){
+	public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getTweet(){
+
+	public String getTweet() {
 		return tweet;
 	}
 
-	public void setTweet(String tweet){
+
+	public void setTweet(String tweet) {
 		this.tweet = tweet;
+	}
+
+	@OneToMany(mappedBy = "tweet")
+	Set<TweetReaction> likes;
+
+	public Set<TweetReaction> getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Set<TweetReaction> likes) {
+		this.likes = likes;
 	}
 
 }
